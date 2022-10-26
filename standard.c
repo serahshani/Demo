7ef49af30d517c9a01fac39b348ac7f0a8acb357
Serah Shani <91112835+serahshani@users.noreply.github.com>
2022-10-26 17:39:42 +0300

    Add files via upload

---
+++ standard.c
@@ -0,0 +1,27 @@
+
+// implementing standard functions in C  
+#include<stdio.h>
+int MYAGE(int birth ,int current);
+int RETIREMENT(int birth); 
+int MYAGE(int birth ,int current) 
+{
+return current_birth;
+}
+int RETIREMENT(int bith)
+{
+return birth +60;
+}
+main()
+{
+int currentyear,birthyear;
+speed=NTSA(699,6);
+printf("********OUTPUT********");
+printf("\n speed=%.2f KM/P\n",speed);
+return 0;
+}
+
+
+
+
+
+
